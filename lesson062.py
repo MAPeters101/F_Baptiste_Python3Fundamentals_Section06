7956fbb902512c77cc12e96a9cd4a0106060616b
Mark Peters <mapeters101@earthlink.net>
2025-02-22 13:48:38 -0500

    Work on lesson062 to 4:00

---
+++ lesson062.py
@@ -0,0 +1,17 @@
+print(ord('A'))
+print(ord('α'))
+print(ord('🐍'))
+
+print(hex(ord('A')))
+print(hex(ord('α')))
+print(hex(ord('🐍')))
+print()
+
+print(int(3.14))
+print(int('3B1', 16))
+print()
+
+α = 'alpha'
+print(α)
+#🐍 = 100
+
