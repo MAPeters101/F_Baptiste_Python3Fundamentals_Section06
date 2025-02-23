310b23abbdce1bfcda76b263404a53e650a79398
Mark Peters <mapeters101@earthlink.net>
2025-02-22 21:36:25 -0500

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,15 @@
+"""
+Question 2
+Using two types of string interpolation, and given the variable a that
+contains an integer, print out the following string for a:
+
+The number ...value of a... is (or is not) even
+
+For example, if a is 42, the your code should print:
+
+'The number 42 is even'
+
+But if a is 31, then the same code should print:
+
+'The number 31 is not even'
+"""
