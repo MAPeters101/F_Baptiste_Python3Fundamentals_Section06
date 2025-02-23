8eb073e8d5bc644d21760551b7c6487c0f4e9222
Mark Peters <mapeters101@earthlink.net>
2025-02-22 21:37:27 -0500

    Description for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,10 @@
+"""
+Question 3
+You are given two variables a and b (with b non-zero), and you need to
+generate a string that reads something like this:
+
+'a / b = (result)'
+But you want your string to be nicely formatted for display purposes, so you
+want to limit displaying possible digits after the decimal point in all your
+values to 4 digits.
+"""
