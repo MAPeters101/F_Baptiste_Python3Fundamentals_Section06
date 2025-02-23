42719423ef0946a97733b204cf38f256e042fe4e
Mark Peters <mapeters101@earthlink.net>
2025-02-22 21:34:03 -0500

    Description for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,20 @@
+"""
+Question 1
+Given this string of comma separated characters, create three new variables
+containing the unicode codepoint (in hex), uppercase and lower case versions
+of each character (also comma delimited).
+
+For example, if the string was 'a, b, c' you should generate three lists that
+look like:
+
+['0x61', '0x62', '0x63']
+['a', 'b', 'c']
+['A', 'B', 'C']
+[You should use the split() and strip() functions, amongst others, to help you
+solve this.]
+
+s = 'Π, ύ, θ, ω, ν'
+"""
+
+
+
