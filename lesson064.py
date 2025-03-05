4fcc713d4aecd6f6a47f88a33c77800bb7496014
Mark Peters <mapeters101@earthlink.net>
2025-03-05 15:08:15 -0500

    Work on lesson064 to 2:10.

---
+++ lesson064.py
@@ -0,0 +1,11 @@
+message = "The definitive guide to Python"
+print(message.upper())
+print(message.lower())
+print(message)
+print(message.title())
+print()
+
+print('abc' == 'ABC')
+print('abc'.lower() == 'ABC'.lower())
+
+
